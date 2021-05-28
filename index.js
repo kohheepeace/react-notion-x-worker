f4767e43294d433c86b80688963eaeea38a1c726
kohheepeace <kohheepeace@gmail.com>
2021-05-29 03:35:56 +0900

    Update error handling

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ const notion = new notionClient.NotionAPI();
 
 app.use(logger("dev"));
 
-app.get("/pages/:id", async (req, res) => {
+app.get("/pages/:id", async (req, res, next) => {
 	const id = req.params.id;
-	const recordMap = await notion.getPage(id);
-	res.json(recordMap);
+	try {
+		const recordMap = await notion.getPage(id);
+		res.json(recordMap);
+	} catch (err) {
+		next(err);
+	}
 });
 
 app.listen(PORT, () => {
 	console.log(`Our app is running on port ${PORT}`);
+});
+
+app.use(function (req, res, next) {
+	res.status(404).send("Invalid URL");
 });
